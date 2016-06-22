405d5b91c7c317ae15a37649cc5b58992db7bb38
Frank Bergmann <frank.bergmann@project-open.com>
2016-06-22 10:38:41 +0000

    - Added "Portfolio" Menu

--- sql/postgresql/intranet-portfolio-management-create.sql
+++ sql/postgresql/intranet-portfolio-management-create.sql
@@ -19,6 +19,72 @@
 -- 300-309              intranet-portfolio-management
 
 
+
+
+-----------------------------------------------------------
+-- Menu for Portfolio Management
+--
+-- Create a menu item and set some default permissions
+-- for various groups who whould be able to see the menu.
+
+
+create or replace function inline_0 ()
+returns integer as $$
+declare
+	v_menu			integer;
+	v_main_menu		integer;
+	v_employees		integer;
+BEGIN
+	select group_id into v_employees from groups where group_name = 'Employees';
+	select menu_id into v_main_menu from im_menus where label = 'main';
+	v_menu := im_menu__new (
+		null, 'im_menu', now(), null, null, null, -- meta information
+		'intranet-portfolio-management',	-- package_name
+		'portfolio',				-- label
+		'Portfolio',				-- name
+		'/intranet-portfolio-management/',	-- url
+		45,					-- sort_order
+		v_main_menu,				-- parent_menu_id
+		null					-- p_visible_tcl
+	);
+	PERFORM acs_permission__grant_permission(v_menu, v_employees, 'read');
+	return 0;
+end;$$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+create or replace function inline_0 ()
+returns integer as $$
+declare
+	v_menu				integer;
+	v_portfolio_menu		integer;
+	v_employees			integer;
+BEGIN
+	select group_id into v_employees from groups where group_name = 'Employees';
+	select menu_id into v_portfolio_menu from im_menus where label = 'portfolio';
+	v_menu := im_menu__new (
+		null, 'im_menu', now(), null, null, null,	-- meta information
+		'intranet-portfolio-management',		-- package_name
+		'strategic_vs_roi',				-- label
+		'Strategic Value vs. ROI',			-- name
+		'/intranet-portfolio-management/strategic-value-vs-roi', -- url
+		45,						-- sort_order
+		v_portfolio_menu,				-- parent_menu_id
+		null						-- p_visible_tcl
+	);
+	PERFORM acs_permission__grant_permission(v_menu, v_employees, 'read');
+	return 0;
+end;$$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+
 -- ----------------------------------------------------------------
 -- Program Portfolio Portlet
 -- ----------------------------------------------------------------
