7967530641ea7f5b36cc2ce9a94411cbaca3c582
Project Open <info@project-open.com>
2021-02-19 18:00:50 +0100

    http -> httpd

--- www/index.tcl
+++ www/index.tcl
@@ -3,6 +3,6 @@
 # Copyright (C) 2003 - 2009 ]project-open[
 #
 # All rights reserved. Please check
-# http://www.project-open.com/license/ for details.
+# https://www.project-open.com/license/ for details.
 
 ad_returnredirect "/intranet-portfolio-management/dashboard"
